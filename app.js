081bf873ec0dd43c08613f8a38bbc0ff6c442e71
BisalBosu <bishalbosu@gmail.com>
2023-05-24 01:03:23 +0530

    adding item to cart

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user')
 
 const app = express();
 
@@ -20,13 +21,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findUserById("646cfa8f82ac6c765bb21b37")
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+  
 });
 
 app.use('/admin', adminRoutes);
@@ -35,5 +36,6 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect(() => {
+   
    app.listen(3000);
 });
--- models/user.js
+++ models/user.js
@@ -2,10 +2,11 @@ const mongodb = require('mongodb')
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(name, email, id) {
+  constructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
-    this._id = _id ? new mongodb.ObjectId(id) : null;
+    this.cart = cart; // {items: []}
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
 
@@ -24,20 +25,50 @@ class User {
       })
   }
 
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp =>{
+      return cp.productId.toString() === product._id.toString();
+    });
+
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if(cartProductIndex >= 0){
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    }
+    else{
+      updatedCartItems.push({ productId: new mongodb.ObjectId(product._id), quantity: newQuantity });
+    }
 
 
-  static findUserByID(userId) {
+
+    const updatedCart = { items: updatedCartItems };
+    
+    const db = getDb();
+    
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: this._id },
+        {
+          $set: { cart: updatedCart }
+        })
+  }
+
+  static findUserById(userId) {
     const db = getDb();
 
-    return db.collection('users').find({ _id: new mongodb.ObjectId(userId)})
-    .next()
-    .then(user => {
-      console.log(user);
-      return user;
-    }).catch(err => {
-      console.log(err);
-    })
-    
+    return db.collection('users')
+      .find({ _id: new mongodb.ObjectId(userId) })
+      .next()
+      .then(user => {
+        console.log(user);
+        return user;
+      }).catch(err => {
+        console.log(err);
+      })
+
 
   }
 
